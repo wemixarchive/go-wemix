37abd86d9f8248ceeef337878c93b7a300071d17
Uh Sado <sadoc@metadium.com>
2018-07-19 18:55:53 -0400

    increased max block size to 100MB from 10MB to accomodate greater gas limit

--- eth/protocol.go
+++ eth/protocol.go
@@ -43,7 +43,7 @@ var ProtocolVersions = []uint{eth63, eth62}
 // ProtocolLengths are the number of implemented message corresponding to different protocol versions.
 var ProtocolLengths = []uint64{17, 8}
 
-const ProtocolMaxMsgSize = 10 * 1024 * 1024 // Maximum cap on the size of a protocol message
+const ProtocolMaxMsgSize = 100 * 1024 * 1024 // Maximum cap on the size of a protocol message
 
 // eth protocol message codes
 const (
