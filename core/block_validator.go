475c038489db05ca58b87c3df6c7e3bae42b01da
Uh Sado <sadoc@metadium.com>
2018-08-24 17:20:51 -0700

    Fixed gas limit setting was not enforced

--- core/block_validator.go
+++ core/block_validator.go
@@ -108,6 +108,10 @@ func (v *BlockValidator) ValidateState(block, parent *types.Block, statedb *stat
 // CalcGasLimit computes the gas limit of the next block after parent.
 // This is miner strategy, not consensus protocol.
 func CalcGasLimit(parent *types.Block) uint64 {
+	if params.FixedGasLimit != 0 {
+		return params.FixedGasLimit
+	}
+
 	// contrib = (parentGasUsed * 3 / 2) / 1024
 	contrib := (parent.GasUsed() + parent.GasUsed()/2) / params.GasLimitBoundDivisor
 
