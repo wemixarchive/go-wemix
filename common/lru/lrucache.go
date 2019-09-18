6bf600bde9d0e019ca031c64826b357701abf4c9
Uh Sado <sadoc@metadium.com>
2019-09-18 07:14:05 +0000

    added lru cache library

--- common/lru/lrucache.go
+++ common/lru/lrucache.go
@@ -1,6 +1,6 @@
 // lrucache.go
 
-package miner
+package lru
 
 import (
 	"container/list"
@@ -9,22 +9,31 @@ import (
 
 type LruCache struct {
 	lock  *sync.RWMutex
-	max   int
-	count int
+	max   int  // max count
+	count int  // current count
+	fifo  bool // if true, element order is not updated on access
 	lru   *list.List
 	data  map[interface{}]interface{}
 }
 
-func NewLruCache(max int) *LruCache {
+// NewLruCache creates LruCache
+func NewLruCache(max int, fifo bool) *LruCache {
 	return &LruCache{
 		lock:  &sync.RWMutex{},
 		max:   max,
 		count: 0,
+		fifo:  fifo,
 		lru:   list.New(),
 		data:  map[interface{}]interface{}{},
 	}
 }
 
+// Count returns the current count of elements
+func (c *LruCache) Count() int {
+	return c.count
+}
+
+// Put adds a key-value pair
 func (c *LruCache) Put(key, value interface{}) {
 	c.lock.Lock()
 	defer c.lock.Unlock()
@@ -49,6 +58,7 @@ func (c *LruCache) Put(key, value interface{}) {
 	}
 }
 
+// Get returns a value with the given key if present, nil otherwise.
 func (c *LruCache) Get(key interface{}) interface{} {
 	c.lock.RLock()
 	defer c.lock.RUnlock()
@@ -58,11 +68,14 @@ func (c *LruCache) Get(key interface{}) interface{} {
 		return nil
 	} else {
 		e := _e.(*list.Element)
-		c.lru.MoveToFront(e)
+		if !c.fifo {
+			c.lru.MoveToFront(e)
+		}
 		return e.Value.([]interface{})[1]
 	}
 }
 
+// Exists checks if a key exists.
 func (c *LruCache) Exists(key interface{}) bool {
 	c.lock.RLock()
 	defer c.lock.RUnlock()
@@ -70,6 +83,7 @@ func (c *LruCache) Exists(key interface{}) bool {
 	return ok
 }
 
+// Del deletes a key-value pair if present. It returns true iff it's present.
 func (c *LruCache) Del(key interface{}) bool {
 	c.lock.Lock()
 	defer c.lock.Unlock()
@@ -85,6 +99,16 @@ func (c *LruCache) Del(key interface{}) bool {
 		c.count--
 		return true
 	}
+}
+
+// Clear resets the lru
+func (c *LruCache) Clear() {
+	c.lock.Lock()
+	defer c.lock.Unlock()
+
+	c.count = 0
+	c.lru = list.New()
+	c.data = map[interface{}]interface{}{}
 }
 
 // EOF
