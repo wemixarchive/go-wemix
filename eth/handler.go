cf0f6d26e006792356335acd510064c578505245
Uh Sado <sadoc@metadium.com>
2021-11-25 06:13:19 +0000

    eth: broadcast transactions to all the peers for now

--- eth/handler.go
+++ eth/handler.go
@@ -486,7 +486,9 @@ func (h *handler) BroadcastTransactions(txs types.Transactions) {
 	for _, tx := range txs {
 		peers := h.peers.peersWithoutTransaction2(tx.Hash())
 		// Send the tx unconditionally to a subset of our peers
-		numDirect := int(math.Sqrt(float64(len(peers))))
+		// numDirect := int(math.Sqrt(float64(len(peers))))
+		// TODO: for now send txs to all the peers
+		numDirect := len(peers)
 		for _, peer := range peers[:numDirect] {
 			txset[peer] = append(txset[peer], tx.Hash())
 		}
