b03596b5fe2b2b931293a027f822cc1153c5eb56
Uh Sado <sadoc@metadium.com>
2022-06-29 19:01:59 +0000

    core, params: wemix testnet genesis & boot nodes

--- params/config.go
+++ params/config.go
@@ -30,7 +30,7 @@ var (
 	MainnetGenesisHash      = common.HexToHash("0xd4e56740f876aef8c010b86a40d5f56745a118d0906a34e69aec8c0db1cb8fa3")
 	RopstenGenesisHash      = common.HexToHash("0x41941023680923e0fe4d74a34bdac8141f2540e3ae90623718e47d66d1ca4a2d")
 	WemixMainnetGenesisHash = common.HexToHash("0xf1b2a543e2dd402fc905d6f2caf9565e796ef83f5356970b2b0a993f01316e27")
-	WemixTestnetGenesisHash = common.HexToHash("0x10c1b0a564a4bffe38bd3cb93ed4df1a0055660093267d6aa6c8d836c992c295")
+	WemixTestnetGenesisHash = common.HexToHash("0x06c0f1b88b9cf5e1d2851af528a1c9408851936520df7be5c6711c019319d68c")
 	SepoliaGenesisHash      = common.HexToHash("0x25a5cc106eea7138acab33231d7160d69cb777ee0c2c553fcddf5138993e6dd9")
 	RinkebyGenesisHash      = common.HexToHash("0x6341fd3daf94b748c72ced5a5b26028f2474f5f00d824504e4fa37a75767e177")
 	GoerliGenesisHash       = common.HexToHash("0xbf7e331f7f7c1dd2e05159666b3bf8bc7a8a3a9eb1d518969eab529dd9b88c1a")
@@ -164,7 +164,7 @@ var (
 		HomesteadBlock:      big.NewInt(0),
 		DAOForkBlock:        big.NewInt(0),
 		DAOForkSupport:      true,
-		EIP150Block:         big.NewInt(1),
+		EIP150Block:         big.NewInt(0),
 		EIP155Block:         big.NewInt(0),
 		EIP158Block:         big.NewInt(0),
 		ByzantiumBlock:      big.NewInt(0),
