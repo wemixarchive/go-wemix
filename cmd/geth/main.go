c4d77a74e5b31fec8e10a36946c2cf03a5667906
rjl493456442 <garyrong0905@gmail.com>
2022-07-25 17:07:44 +0800

    cmd: use flags.Merge for grouping flags (#25392)

--- cmd/geth/main.go
+++ cmd/geth/main.go
@@ -63,7 +63,7 @@ var (
 	// The app that holds all commands and flags.
 	app = flags.NewApp(gitCommit, gitDate, "the go-wemix command line interface")
 	// flags that configure the node
-	nodeFlags = utils.GroupFlags([]cli.Flag{
+	nodeFlags = flags.Merge([]cli.Flag{
 		utils.IdentityFlag,
 		utils.UnlockedAccountFlag,
 		utils.PasswordFileFlag,
@@ -269,7 +269,7 @@ func init() {
 	}
 	sort.Sort(cli.CommandsByName(app.Commands))
 
-	app.Flags = utils.GroupFlags(
+	app.Flags = flags.Merge(
 		nodeFlags,
 		rpcFlags,
 		consoleFlags,
