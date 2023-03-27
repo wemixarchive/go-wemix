d35abad8c791eab0b3ebf82579aadfb9ddb3287c
coinplug-jhjin <jhjin@coinplug.com>
2023-03-27 17:00:01 +0900

    Revert "internal/ethapi: add error handling for invalid block hash (GetReceiptsByHash)"
    This reverts commit 3bfaa5a50d769e05afb8efdc586ca77024f0595a.

--- internal/ethapi/api.go
+++ internal/ethapi/api.go
@@ -649,9 +649,7 @@ func (s *PublicBlockChainAPI) GetReceiptsByHash(ctx context.Context, blockHash c
 		return nil, err1
 	}
 	block, err2 := s.b.BlockByHash(ctx, blockHash)
-	if block == nil {
-		return nil, errors.New("block does not exist in blockchain")
-	} else if err2 != nil {
+	if err2 != nil {
 		return nil, err2
 	}
 
