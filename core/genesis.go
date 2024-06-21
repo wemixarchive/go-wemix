d9615ef3372c6456dc78b5ca34000afbd5bd4032
egon-wm <egon@wemade.com>
2024-06-21 09:54:45 +0900

    feat: brioche configuation for devnet (#108)
    * feat: add testable private chain for brioche
    
    * fix: apply devnet config
    
    * ci: add ci for devnet
    
    * fix: remove local test

--- core/genesis.go
+++ core/genesis.go
@@ -302,7 +302,10 @@ func SetupGenesisBlockWithOverride(db ethdb.Database, genesis *Genesis, override
 	// chain config as that would be AllProtocolChanges (applying any new fork
 	// on top of an existing private network genesis block). In that case, only
 	// apply the overrides.
-	if genesis == nil && !(stored == params.MainnetGenesisHash || stored == params.WemixMainnetGenesisHash) {
+	if genesis == nil &&
+		!(stored == params.MainnetGenesisHash ||
+			stored == params.WemixMainnetGenesisHash ||
+			stored == params.DevnetGenesisHash) {
 		newcfg = storedcfg
 		if overrideArrowGlacier != nil {
 			newcfg.ArrowGlacierBlock = overrideArrowGlacier
@@ -327,6 +330,8 @@ func SetupGenesisBlockWithOverride(db ethdb.Database, genesis *Genesis, override
 
 func (g *Genesis) configOrDefault(ghash common.Hash) *params.ChainConfig {
 	switch {
+	case ghash == params.DevnetGenesisHash:
+		return params.WemixDevnetChainConfig
 	case ghash == params.WemixMainnetGenesisHash:
 		return params.WemixMainnetChainConfig
 	case ghash == params.WemixTestnetGenesisHash:
