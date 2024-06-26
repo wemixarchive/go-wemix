182458f751f830282c702fa0b17a930d99e7c926
paul-ahn-wm <paulahn2022@wemade.com>
2024-06-26 13:53:16 +0900

    chore: improve waiting message for port to open in init-boot.sh

--- wemix/scripts/init-boot.sh
+++ wemix/scripts/init-boot.sh
@@ -5,9 +5,26 @@ bin/gwemix.sh start &
 
 wait_for_port() {
     local port="$1"
-    while ! nc -z localhost "$port"; do
-        echo "Waiting for port $port to open..."
-        sleep 1
+    local retry_interval=1
+    local max_retries=30
+    local retries=0
+
+    while :; do
+        # wget을 사용하여 localhost의 특정 포트에 HTTP 요청을 시도합니다.
+        # -T 옵션은 타임아웃을 설정합니다. -O -는 출력을 무시합니다.
+        if wget --spider -T 10 -O - http://localhost:"$port" >/dev/null 2>&1; then
+            echo "Port $port is open."
+            break
+        else
+            echo "Waiting for port $port to open. Attempt $((retries+1))/$max_retries."
+            sleep "$retry_interval"
+        fi
+
+        retries=$((retries+1))
+        if [ "$retries" -ge "$max_retries" ]; then
+            echo "Failed to connect to port $port after $max_retries attempts."
+            exit 1
+        fi
     done
 }
 
