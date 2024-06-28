07bdd13053244f9b18f93d26e39262ed8022a575
paul-ahn-wm <paulahn2022@wemade.com>
2024-06-28 11:51:00 +0900

    chore: Update init-boot.sh to use console command for etcd initialization

--- wemix/scripts/init-boot.sh
+++ wemix/scripts/init-boot.sh
@@ -40,4 +40,9 @@ bin/gwemix.sh init-gov "" conf/config.json keystore/account1 test
 echo "sleep 10 seconds for etcd init..."
 sleep 10
 
-curl -X POST --data '{"jsonrpc":"2.0","method":"admin_etcdInit","params":[],"id":1}' -H "Content-Type: application/json" http://127.0.0.1:8598
+# Open console and execute admin.etcdInit()
+bin/gwemix.sh console <<EOF
+admin.etcdInit()
+EOF
+
+echo "etcd init done."
