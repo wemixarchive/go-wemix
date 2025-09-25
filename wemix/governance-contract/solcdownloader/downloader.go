8350f56d266ed27d7f221898be525bb0bc555054
eomti-wm <eomti@wemade.com>
2025-09-25 12:07:02 +0900

    fix: apply comment

--- wemix/governance-contract/solcdownloader/downloader.go
+++ wemix/governance-contract/solcdownloader/downloader.go
@@ -1,3 +1,19 @@
+// Copyright 2025 The go-wemix-wbft Authors
+// This file is part of the go-wemix-wbft library.
+//
+// The go-wemix-wbft library is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// The go-wemix-wbft library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with the go-wemix-wbft library. If not, see <http://www.gnu.org/licenses/>.
+
 package solcdownloader
 
 import (
